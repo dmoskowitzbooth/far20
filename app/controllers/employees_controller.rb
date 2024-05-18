57140b34d67c3fd0c3f8475a075c2f2f4bf04274
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-18 21:24:08 +0000

    1

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -23,6 +23,11 @@ class EmployeesController < ApplicationController
     matching_disciplines_exp = Discipline.where({ :emp_id => the_id }).where('expires < ?', today)
     @list_of_disciplines = matching_disciplines.order({ :expires => :desc })
     @list_of_disciplines_exp = matching_disciplines_exp.order({ :expires => :desc })
+    
+    @max_disciplines = @list_of_disciplines.order(level: :desc).first
+
+    @max_disciplines_exp = @list_of_disciplines.order(level: :desc).first
+
 
     render({ :template => "employees/show" })
   end

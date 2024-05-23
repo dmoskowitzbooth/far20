953aeca61dc65eb0f4257e163ce87ab26aa1153d
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-23 17:12:05 +0000

    rev

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # Routes for the Fact finding resource:
 
   # CREATE
@@ -101,6 +103,8 @@ Rails.application.routes.draw do
 
   #------------------------------
     Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # Routes for the Fact finding resource:
 
   # CREATE
